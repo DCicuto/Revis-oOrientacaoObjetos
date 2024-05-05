2aca8486a7cbfce84f86ff2ac00f8a0814940158
Daiane Costa <cicuto.adv@gmail.com>
2024-05-05 00:59:58 -0300

    Revisão Orientação a Objetos

---
+++ RevisãoOrientacaoObjetos/Pessoa.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevisãoOrientacaoObjetos
+{
+    internal class Pessoa
+    {
+        string nome;  
+        int idade;
+
+        public Pessoa()
+        {
+            nome = "John Doe";
+            idade = 0;
+            Console.WriteLine("Classe criou a pessoa:");
+        }
+
+        public Pessoa(string nome, int idade)
+        {
+            this.nome = nome;
+            this.idade = idade;
+        }
+         
+        public void setNome(string nome)//metodo de atribuiçao para o nome
+        {
+            this.nome = nome;
+        } 
+
+        public void setIdade(int idade)
+        {
+            this.idade = idade;
+        }
+        
+        public string getNome()
+        {
+            return nome;
+        }
+        public int getIdade()
+        {
+            return idade;
+        }
+    }
+}
--- RevisãoOrientacaoObjetos/Program.cs
+++ RevisãoOrientacaoObjetos/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace RevisãoOrientacaoObjetos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa("Jorge", 39); //aqui falo que p1 vai receber uma nova pessoa, e é aqui que instacio o objeto pessoa, o comando new é que manda criar o obejeto na memoria 
+            Console.WriteLine("Os dados da pessoa p1 criada são:");
+            Console.WriteLine(p1.getNome());
+            Console.WriteLine(p1.getIdade());
+            //p1 é o controlador do tipo da classse Pessoa com P maisculo 
+            // Apenas para enteder: acima temos dois objetos o p1 e o pessoa 
+
+            Pessoa pessoa = new Pessoa();
+            Console.WriteLine("Os dados da pessoa criada são:");
+            Console.WriteLine(pessoa.getNome());
+            Console.WriteLine(pessoa.getIdade());
+            //com os metodos get e set pegamos o objeto pessoa, e conseguimos acessar o atributo nome dele e atribuir o nome dele 
+
+            pessoa.setNome("Pestana");
+            pessoa.setIdade(38);
+
+            Console.WriteLine("Os dados da pessoa criada são:");
+            Console.WriteLine("Nome:");
+            Console.WriteLine(pessoa.getNome());
+            Console.WriteLine("Idade:");
+            Console.WriteLine(pessoa.getIdade());
+
+
+
+        }
+
+       
+    }
+
+
+
+
+}
+ 
